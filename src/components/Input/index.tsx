a931cd0e7f13c273e43ca3749d38de88fd9fa501
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2020-08-23 19:40:49 -0300

    Melhorando o feedback de foco e preenchimento no input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,6 +3,8 @@ import React, {
   useRef,
   useImperativeHandle,
   forwardRef,
+  useState,
+  useCallback,
 } from 'react';
 import { TextInputProps } from 'react-native';
 import { useField } from '@unform/core';
@@ -13,11 +15,9 @@ interface InputProps extends TextInputProps {
   name: string;
   icon: string;
 }
-
 interface InputValueReference {
   value: string;
 }
-
 interface InputRef {
   focus(): void;
 }
@@ -30,6 +30,25 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
 
   const inputElementRef = useRef<any>(null);
   const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    setIsFilled(!!inputValueRef.current.value);
+    /*
+    if (inputValueRef.current.value) {
+      setIsFilled(true);
+    }
+
+    setIsFilled(false);
+    */
+  }, []);
 
   useImperativeHandle(ref, () => ({
     focus() {
@@ -55,13 +74,19 @@ const Input: React.RefForwardingComponent<InputRef, InputProps> = (
   }, [fieldName, registerField]);
 
   return (
-    <Container>
-      <Icon name={icon} size={20} color="#666360" />
+    <Container isFocused={isFocused}>
+      <Icon
+        name={icon}
+        size={20}
+        color={isFocused || isFilled ? '#ff9000' : '#666360'}
+      />
       <TextInput
         ref={inputElementRef}
         keyboardAppearance="dark"
         placeholderTextColor="#666360"
         defaultValue={defaultValue}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         onChangeText={value => {
           inputValueRef.current.value = value;
         }}
